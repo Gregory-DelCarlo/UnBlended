30440754e7592262a9d4419f2ae79a71692e628b
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-03-30 16:11:11 -0700

    Styled drinks index to match Untappd

--- frontend/components/drinks/drinks_index_item.jsx
+++ frontend/components/drinks/drinks_index_item.jsx
@@ -14,9 +14,19 @@ export default class DrinksIndexItem extends React.Component {
         return (
             <Link to={`/drinks/${drink.id}`} className='link' >
                 <li className='drinks-item'>
-                    <div>
-                        <p>{drink.name}</p>
-                        <p>{drink.type}</p>
+                    <div className='drink-details'>
+                        <p id='name'>{drink.name}</p>
+                        <p id='dist'>Distillery!</p>
+                        <p id='type'>type: {drink.type}</p>
+                    </div>
+                    <div className='drink-info'>
+                        <div className='drink-percents'>
+                            <p id='abv'>{drink.abv}% ABV</p>
+                            <p id='proof'>{drink.proof} proof</p>
+                        </div><br/>
+                        <div className='drink-rating'>
+                            <p id='rating'>Rating</p>
+                        </div>
                     </div>
                 </li>
             </Link>
