41d8bfba2806d849ff43c14d70b42843533cbd4c
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-04-07 15:12:32 -0700

    cleaned up unneccessary imports and comments and added logger to Redux store

--- frontend/unblended.jsx
+++ frontend/unblended.jsx
@@ -2,15 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
-// import { 
-//     login, logout, signup 
-// } from './util/session_api_util'
-import { 
-    getDrinks
-} from './actions/whiskey_actions';
-// import {
-//     createNewUser, loginUser, logoutUser
-// } from './actions/session/session_actions';
+
 
 let preLoadedState = {}
 
@@ -32,11 +24,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     window.store = store;
-    // window.login = login;
-    // window.logout = logout;
-    // window.signup = signup;
-    // window.login = loginUser;
-    // window.logout = logoutUser;
-    // window.signup = createNewUser;
-    // window.getDrinks = getDrinks;
 });
