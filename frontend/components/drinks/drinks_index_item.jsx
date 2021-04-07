09837736b206459e08355d071c5d887d5bfc6e1d
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-04-06 20:02:01 -0700

    Polished the look of the drinks list

--- frontend/components/drinks/drinks_index_item.jsx
+++ frontend/components/drinks/drinks_index_item.jsx
@@ -12,19 +12,21 @@ export default class DrinksIndexItem extends React.Component {
         const {drink} = this.props;
         return (
             <Link to={`/drinks/${drink.id}`} className='link drinks-index' >
-                <img src={drink.photo} alt='drink image'/>
+                <div className='drink-img-container'>
+                    <img src={drink.photo} alt='drink image'/>
+                </div>
                 <li className='drinks-item'>
                     <div className='drink-details'>
                         <p id='name'>{drink.name}</p>
                         <p id='dist'>Distillery!</p>
-                        <p id='type'>type: {drink.type}</p>
+                        <p id='type' style={{color: 'grey'}}>type: {drink.type}</p>
                     </div>
                     <div className='drink-info'>
                         <div className='drink-percents'>
                             <p id='abv'>{drink.abv}% ABV</p>
                             <p id='proof'>{drink.proof} proof</p>
                         </div><br/>
-                        <div className='drink-rating'>
+                        <div className='drink-rating'> 
                             <p id='rating'>Rating</p>
                         </div>
                     </div>
