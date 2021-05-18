f6804a4ff9780d136a2cf163a1f577766398bedc
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-05-18 15:19:30 -0700

    Added Delete button to reviews

--- frontend/components/reviews/reviews_index_item.jsx
+++ frontend/components/reviews/reviews_index_item.jsx
@@ -8,12 +8,70 @@ export default class ReviewsIndexItem extends React.Component {
         this.props.getDrink(this.props.review.whiskey);
         this.props.getUser(this.props.review.user);
         this.props.getDistillery(this.props.whiskey.distillery);
+
+        this.handleDelete = this.handleDelete.bind(this);
+        this.confirmDelete = this.confirmDelete.bind(this);
+        this.checkDelete = this.checkDelete.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+        this.checkModalClose = this.checkModalClose.bind(this);
+
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    openModal() {
+        this.setState({isOpen: true});
+    }
+
+    closeModal() {
+        this.setState({isOpen: false});
+    }
+
+    checkModalClose(e) {
+        if(e.target.className == 'modal') {
+            this.closeModal();
+        }
+    }
+
+    handleDelete() {
+        this.props.deleteReview(this.props.review.id, this.props.currentUser)
+    }
+
+    confirmDelete() {
+        const {isOpen} = this.state;
+        
+        return (
+            <div className={isOpen ? 'open' : 'closed'} onClick={this.checkModalClose}>
+                <div className='modal'>
+                    <div className='modal-delete-box'>
+                        <div id='title'>Confirm Check-In Deletion</div>
+                        <div id='disclaim'><span>Warning </span>
+                        - Are you sure you want to delete this check-in? 
+                        This cannot be undone.</div>
+                        <div id='delete'><button onClick={this.handleDelete}>Confirm Deletion</button></div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    checkDelete() {
+        const {currentUser, review} = this.props;
+
+        if(currentUser === review.user) {
+            return(
+                <span id='delete'> <button onClick={this.openModal}>Delete Check-in</button> </span>
+            )
+        }
     }
 
     render() {
         const {review, whiskey, user, distillery} = this.props;
         return (
             <div className='reviews-index' >
+                {this.confirmDelete()}
                 <div className='review-head'>
                     <div className='review-title'>
                         <Link to=''>{user.username} </Link>
@@ -31,6 +89,10 @@ export default class ReviewsIndexItem extends React.Component {
                 <div className='review-details'>
                     <div className='review-body'>{review.body}</div>
                     <div>rating: {review.rating} stars</div>
+                </div>
+                <div className='review-footer'>
+                    <span id='detailed'>View Detailed Check-In</span>
+                    {this.checkDelete()}
                 </div>
             </div>
         )
--- frontend/components/reviews/reviews_index_item_container.js
+++ frontend/components/reviews/reviews_index_item_container.js
@@ -3,20 +3,23 @@ import reviewsIndexItem from './reviews_index_item';
 import { getDrink } from '../../actions/whiskey_actions';
 import { getUser } from '../../actions/user_actions';
 import { getDistillery } from '../../actions/distillery_actions';
+import { destroyReview } from '../../actions/review_actions';
 
 const mapStateToProps = (state, ownProps) => {{
     const whiskey = state.entities.whiskey[ownProps.review.whiskey];
     return ({
         whiskey:  whiskey ? whiskey : '',
         distillery: state.entities.distilleries[whiskey.distillery] ? state.entities.distilleries[whiskey.distillery] : '',
-        user: state.entities.users[ownProps.review.user] ? state.entities.users[ownProps.review.user] : ''
+        user: state.entities.users[ownProps.review.user] ? state.entities.users[ownProps.review.user] : '',
+        currentUser: state.session.id
     });
 }};
 
 const mapDispatchToProps = dispatch => ({
     getDrink: drinkId => dispatch(getDrink(drinkId)),
     getUser: userId => dispatch(getUser(userId)),
-    getDistillery: distId => dispatch(getDistillery(distId))
+    getDistillery: distId => dispatch(getDistillery(distId)),
+    deleteReview: (reviewId, userId) => dispatch(destroyReview(reviewId, userId))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(reviewsIndexItem);
