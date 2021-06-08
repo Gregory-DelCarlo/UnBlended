c933bee15f5350e180a57806b791028c36d13a2f
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-06-07 20:06:30 -0700

    Added routes for Cheers and Comments

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,13 @@ Rails.application.routes.draw do
     resources :distilleries, only: [:index, :show]
     resources :reviews, only: [:index, :show, :create, :destroy, :update]
     resources :friends, only: [:index, :create]
+    resources :comments, only: [:create, :update, :destroy]
 
     get '/single_drink_rating', :to => 'reviews#single_drink_rating'
     get '/all_drink_ratings', :to => 'reviews#all_drink_ratings'
     delete '/friends', :to => 'friends#destroy'
-    
+    post '/toggle_cheers', :to => 'cheers#toggle_cheers'
+    get '/all_cheers', :to => 'cheers#index'
+    get '/cheers', :to => 'cheers#show'
   end
 end
